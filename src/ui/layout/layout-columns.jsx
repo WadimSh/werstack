2cb2d433e3debd0f04916dca6ac8dfdb6b466246
WadimSh <wadim11@yandex.ru>
2023-11-08 11:20:23 +0300

    fix: оптимизирован элемент определяющий лайаут

--- src/ui/layout/layout-columns.jsx
+++ src/ui/layout/layout-columns.jsx
@@ -1,56 +1,40 @@
-import React, { useCallback, useMemo } from "react";
+import React, { useMemo } from "react";
 import './layout-columns.css';
 
-const validNames = ['section', 'div', 'article', 'footer', 'header'];
-const breakpoints = {
-  xs: 0,
-  sm: 600,
-  md: 960,
-  lg: 1280,
-  xl: 1920,
-};
-
 const LayoutColumns = ({ container, item, nameTag, forwardedRef, gap, children, xs, sm, md, lg, xl }) => {
+  const validNames = ['section', 'div', 'article', 'footer', 'header'];
   const Tag = validNames.includes(nameTag) ? nameTag : 'div';
+  const defaultSize = 12;
 
-  //Заготовка новому варианту функции
-  //const defaultSize = 12;
-  //const sizes = {xs, sm, md, lg, xl} = {
-  //  xs: xs !== undefined ? xs : defaultSize,
-  //  sm: sm !== undefined ? sm : xs || defaultSize,
-  //  md: md !== undefined ? md : sm || defaultSize,
-  //  lg: lg !== undefined ? lg : md || defaultSize,
-  //  xl: xl !== undefined ? xl : lg || defaultSize,
-  //};
-  
-  const getBreakpointSize = useCallback((size) => {
-    return size <= 12 ? size : 12;
-  }, []);
-
-  const itemStyle = useMemo(() => {
-    if (!item) return {};
-    let size = getBreakpointSize(xs);
-    const sizes = {xs, sm, md, lg, xl};
-    for (const [key, value] of Object.entries(breakpoints)) {
-      if (window.matchMedia(`(min-width: ${value}px)`).matches) {
-        size = getBreakpointSize(sizes[key]);
-      }
-    }
-    return { gridColumn: `span ${size}` };
-  }, [xs, sm, md, lg, xl, item, getBreakpointSize]);
-
-  const containerStyle = useMemo(() => {
-    return container ? { gap: gap + 'px' } : {};
-  }, [container, gap]);
+  const sizes = useMemo(() => {
+    return {
+      xs: xs || defaultSize,
+      sm: sm || xs || defaultSize,
+      md: md || sm || xs || defaultSize,
+      lg: lg || md || sm || xs || defaultSize,
+      xl: xl || lg || md || sm || xs || defaultSize,
+    };
+  }, [xs, sm, md, lg, xl]);
 
   const combinedStyle = useMemo(() => {
-    return { ...containerStyle, ...itemStyle };
-  }, [containerStyle, itemStyle]);
+    return {
+      ...(container ? { '--gap-gap': gap + 'px' } : {}),
+      ...(item
+        ? {
+            '--xs': sizes.xs,
+            '--sm': sizes.sm,
+            '--md': sizes.md,
+            '--lg': sizes.lg,
+            '--xl': sizes.xl,
+          }
+        : {}),
+    };
+  }, [container, gap, item, sizes]);
 
   return (
-    <Tag
-      style={combinedStyle}
-      className={container ? 'container' : item ? 'item' : ''}
+    <Tag 
+      style={combinedStyle} 
+      className={container ? 'container' : item ? 'item' : ''} 
       ref={forwardedRef}
     >
       {children}
